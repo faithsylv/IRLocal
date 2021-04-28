e0e4f1f84a8ba24d41933771b507d3492b080545
faithsylvia1 <you@example.com>
2021-04-28 14:30:07 +1000

    amendments to search and add geocoder

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,3 @@
 class SearchController < ApplicationController
 
-  def index
-    if params[:query].present?
-      name = params[:query]
-      if Brand.where("name ilike ?", "%#{name}%").present?
-        @header = "Stores selling this brand:"
-        brand = Brand.where("name ilike ?", "%#{name}%") #get array of possible brands
-        @shops = brand.first.shops #take brand outside of array and get shops
-      elsif Category.where("name ilike ?", "%#{name}%").present?
-        @header = "Stores selling this category of goods:"
-        category = Category.where("name ilike ?", "%#{name}%")
-        @shops = category.first.shops
-      elsif Shop.where("name ilike ?", "%#{name}%").present?
-        @header = "Stores:"
-        @shops = Shop.where("name ilike ?", "%#{name}%")
-      else
-        @return_text = "Sorry, we couldn't find your a store that sells that category or brand."
-        @shops = Shop.all
-      end
-    end
-  end
 end
