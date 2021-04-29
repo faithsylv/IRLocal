d88e7c3363bb1d4405ef9d9ad2eb0d6aecaf0942
faithsylvia1 <you@example.com>
2021-04-29 10:16:30 +1000

    drop search table

---
+++ db/migrate/20210429001244_drop_search_table.rb
@@ -0,0 +1,9 @@
+class DropSearchTable < ActiveRecord::Migration[5.2]
+  def up
+   drop_table :pg_search_documents
+  end
+
+ def down
+   raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_005947) do
+ActiveRecord::Schema.define(version: 2021_04_29_001244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,15 +38,6 @@ ActiveRecord::Schema.define(version: 2021_04_27_005947) do
   create_table "categories_shops", id: false, force: :cascade do |t|
     t.integer "category_id"
     t.integer "shop_id"
-  end
-
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text "content"
-    t.string "searchable_type"
-    t.bigint "searchable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "shops", force: :cascade do |t|
